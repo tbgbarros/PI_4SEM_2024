5432b5cffe3af85edf4d6a89fc246947983a76f0
pedr0tavi0 <112214113+pedr0tavi0@users.noreply.github.com>
2024-03-27 20:34:26 -0300

    alteração no model, para tratar hashs

--- CODIFICAÇÃO/app/models/user.py
+++ CODIFICAÇÃO/app/models/user.py
@@ -1,3 +1,4 @@
+import binascii
 from app import db
 from cryptography.fernet import Fernet
 import base64
@@ -19,6 +20,11 @@ class User(db.Model):
         self.hash = base64.urlsafe_b64encode(key).decode()  # Store the key as a base64 encoded string
 
     def check_password(self, password):
-        cipher_suite = Fernet(base64.urlsafe_b64decode(self.hash.encode()))  # Use the stored key, decoding it
-        decrypted_password = cipher_suite.decrypt(self.password_encrypted).decode()
-        return password == decrypted_password
+        try:
+            key_bytes = base64.urlsafe_b64decode(self.hash.encode())
+            cipher_suite = Fernet(key_bytes)
+            decrypted_password = cipher_suite.decrypt(self.password_encrypted).decode()
+            return password == decrypted_password
+        except (binascii.Error, ValueError):
+            # Tratar exceções relacionadas à decodificação de base64 ou chave inválida
+            return False
