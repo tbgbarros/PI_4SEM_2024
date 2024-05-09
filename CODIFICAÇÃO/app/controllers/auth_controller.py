652099c5a1e92b6b220fc24970bd3654d9860210
pedr0tavi0 <p.ocamargol@gmail.com>
2024-05-08 22:12:12 -0300

    rotas formadas para a api

--- CODIFICAÇÃO/app/controllers/auth_controller.py
+++ CODIFICAÇÃO/app/controllers/auth_controller.py
@@ -24,5 +24,5 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('auth.login'))
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.principal'))
     return render_template('auth/login.html', title='Sign In', form=form)
--- CODIFICAÇÃO/app/controllers/main_controller.py
+++ CODIFICAÇÃO/app/controllers/main_controller.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
 
-bp = Blueprint('main', __name__)
+from flask import Blueprint, render_template, redirect, url_for, flash
+from app.forms import SignupForm, LoginForm
+
+bp = Blueprint('main', __name__,template_folder='templates/main')
 
 @bp.route('/')
 def index():
-    return render_template('main/index.html')
+    return render_template('index.html')
+
+@bp.route('/principal')
+def principal():
+    return render_template('main/principal.html')
