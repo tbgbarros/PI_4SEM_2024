22967b387b8f1e226aff256ef260e4443a63997d
tbgbarros <tbg.barros@hotmail.com>
2024-05-12 01:53:21 -0300

    alteracao mensagem de login e layout

--- CODIFICAÇÃO/app/controllers/auth_controller.py
+++ CODIFICAÇÃO/app/controllers/auth_controller.py
@@ -3,35 +3,37 @@ from app.forms import SignupForm, LoginForm
 from app.models.user import User
 from app import db
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-@bp.route('/signup', methods=['GET', 'POST'])
+
+@bp.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('auth.login'))
-    return render_template('auth/signup.html', title='Sign Up', form=form)
+        flash("Cadastrado, agora voce pode entrar para o Clã!")
+        return redirect(url_for("auth.login"))
+    return render_template("auth/signup.html", title="Sign Up", form=form)
 
-@bp.route('/login', methods=['GET', 'POST'])
+
+@bp.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('auth.login'))
-        session['username'] = form.username.data
-        return redirect(url_for('main.principal'))
-    return render_template('auth/login.html', title='Sign In', form=form)
+            flash("Invalid username or password")
+            return redirect(url_for("auth.login"))
+        session["username"] = form.username.data
+        return redirect(url_for("main.principal"))
+    return render_template("auth/login.html", title="Sign In", form=form)
 
 
-
-
-@bp.route('/logout')
+@bp.route("/logout")
 def logout():
-    session.pop('username', None)
-    return redirect(url_for('main.index'))  # Redireciona para a rota principal após o logout
+    session.pop("username", None)
+    return redirect(
+        url_for("main.index")
+    )  # Redireciona para a rota principal após o logout
