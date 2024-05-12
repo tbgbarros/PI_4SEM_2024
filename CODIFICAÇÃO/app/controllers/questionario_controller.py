1a68397a167a9432b14d058aa4a1cc0c0c3b74a9
tbgbarros <tbg.barros@hotmail.com>
2024-05-11 23:02:07 -0300

    enviar

--- CODIFICAÇÃO/app/controllers/questionario_controller.py
+++ CODIFICAÇÃO/app/controllers/questionario_controller.py
@@ -5,17 +5,19 @@ from app.models.openia_questionario import OpeniaQuestionario
 openia_questionario = OpeniaQuestionario(api_key="")
 
 
-bp = Blueprint('quest', __name__, url_prefix='/quest')
+bp = Blueprint("quest", __name__, url_prefix="/quest")
 
-@bp.route('/gerar', methods=['POST'])
+
+@bp.route("/gerar", methods=["POST"])
 def gerar_questionario():
-    tema = request.form['tema']
+    tema = request.form["tema"]
     perguntas = openia_questionario.gerar_perguntas(tema)
-    return render_template('quest/gerar.html', perguntas=perguntas)
+    return render_template("quest/gerar.html", perguntas=perguntas)
 
-@bp.route('/processar', methods=['POST'])
+
+@bp.route("/processar", methods=["POST"])
 def processar_respostas():
     resposta = request.form
     respostas = openia_questionario.verificar_respostas(resposta)
     print(respostas)
-    return render_template('quest/processar.html', respostas=respostas)
+    return render_template("quest/processar.html", respostas=respostas)
