5ef833e600351cc5f16336f3cd54c918c7244665
tbgbarros <111811766+tbgbarros@users.noreply.github.com>
2024-05-29 13:49:05 -0300

    actions mysql erro

--- CODIFICAÇÃO/app/__init__.py
+++ CODIFICAÇÃO/app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import inspect
 import bcrypt
 from flask import Flask
@@ -6,40 +7,42 @@ from flask_migrate import Migrate
 from config import Config
 from flask_bcrypt import Bcrypt
 from sqlalchemy_utils import database_exists, create_database
-from config import Config
 import logging
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 migrate = Migrate()
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config['SECRET_KEY'] = 'pedro'
+    app.config["SECRET_KEY"] = "pedro"
 
+    # Usa DATABASE_URL da variável de ambiente ou pega da configuração
+    db_uri = os.getenv("DATABASE_URL", app.config["SQLALCHEMY_DATABASE_URI"])
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 
-    db_uri = app.config['SQLALCHEMY_DATABASE_URI']
     if not database_exists(db_uri):
         create_database(db_uri)
- 
+
     db.init_app(app)
     bcrypt.init_app(app)
     migrate.init_app(app, db)
 
     with app.app_context():
-         from.models.user import User
-         db.create_all()
-    
+        from .models.user import User
 
+        db.create_all()
 
-    
+    from app.controllers import (
+        auth_controller,
+        main_controller,
+        questionario_controller,
+    )
 
-    from app.controllers import auth_controller, main_controller, questionario_controller
     app.register_blueprint(auth_controller.bp)
     app.register_blueprint(main_controller.bp)
     app.register_blueprint(questionario_controller.bp)
-
-   
 
     return app
--- CODIFICAÇÃO/run.py
+++ CODIFICAÇÃO/run.py
@@ -1,7 +1,9 @@
+import os
+from sqlalchemy import create_engine
 from app import create_app
 
 app = create_app()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
