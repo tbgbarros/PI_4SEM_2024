cbedf7f7bc58053193ba2cc3dce4e8498a44c222
tbgbarros <111811766+tbgbarros@users.noreply.github.com>
2024-05-29 12:49:37 -0300

    alteracao na chave api e criacao do github actions

--- CODIFICAÇÃO/app/controllers/questionario_controller.py
+++ CODIFICAÇÃO/app/controllers/questionario_controller.py
@@ -1,10 +1,19 @@
+import os
 from flask import render_template, request, session
 from flask import Blueprint, redirect, url_for, flash
+from dotenv import load_dotenv
 from app.models.openia_questionario import OpeniaQuestionario
 
-openia_questionario = OpeniaQuestionario(api_key="")
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+# Obtém a chave de API do ambiente
+api_key = os.getenv("CHATGPT_API_KEY")
+
+openia_questionario = OpeniaQuestionario(api_key=api_key)
 
 bp = Blueprint("quest", __name__, url_prefix="/quest")
+
 
 @bp.route("/gerar", methods=["POST"])
 def gerar_questionario():
@@ -15,25 +24,31 @@ def gerar_questionario():
     respostas = openia_questionario.obter_respostas_chat(tema)
     print(f"Respostas do chat na rota gerar_questionario: {respostas}")
 
-    session['respostas'] = respostas
+    session["respostas"] = respostas
     return render_template("quest/gerar.html", perguntas=perguntas)
-
 
 
 @bp.route("/processar", methods=["POST"])
 def processar_respostas():
     respostas_submetidas = request.form
-    respostas_corretas = session.get('respostas')
+    respostas_corretas = session.get("respostas")
+    username = session.get("username")
 
     print(f"Respostas submetidas dentro do processar_respostas: {respostas_submetidas}")
-    print(f"Respostas corretas fornecidas pela API dentro do processar respostas: {respostas_corretas}")
+    print(
+        f"Respostas corretas fornecidas pela API dentro do processar respostas: {respostas_corretas}"
+    )
 
     if respostas_corretas:
-        resultados, acertos = openia_questionario.verificar_respostas(respostas_submetidas, respostas_corretas)
+        resultados, acertos = openia_questionario.verificar_respostas(
+            respostas_submetidas, respostas_corretas, username
+        )
+
         print(f"Resultados: {resultados}, Acertos: {acertos}")
     else:
         resultados = None
         acertos = 0
 
-    return render_template("quest/processar.html", resultados=resultados, acertos=acertos)
-
+    return render_template(
+        "quest/processar.html", resultados=resultados, acertos=acertos
+    )
