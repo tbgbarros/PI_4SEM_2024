0797b43243883afaf5bbe20a7999bd120ea1eecf
tbgbarros <tbg.barros@hotmail.com>
2024-05-29 21:47:12 -0300

    git ignore e virtual bat

--- CODIFICAÇÃO/virtualvenv2.0.bat
+++ CODIFICAÇÃO/virtualvenv2.0.bat
@@ -13,14 +13,14 @@ call venv\Scripts\activate
 
 :: Instala as dependências do projeto a partir do arquivo requirements.txt
 pip install -r requirements.txt
-
+code .
 python run.py
 
 :: Exibe mensagem de conclusão
 @REM echo Ambiente virtual Python criado e ativado em "%default_folder%\project_temp\venv"
 
 @REM ::Abre vscode
-@REM code .
+
 
 @REM ::exit
 @REM exit /b 0
