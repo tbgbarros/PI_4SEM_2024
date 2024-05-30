8e64d77aba7cf36aeeace77ee29c6b996fc3fe43
tbgbarros <tbg.barros@hotmail.com>
2024-05-29 23:58:58 -0300

    tdd

--- CODIFICAÇÃO/config.py
+++ CODIFICAÇÃO/config.py
@@ -1,10 +1,22 @@
+# import os
+# import app
+
+
+# class Config:
+#     SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key"
+#     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/flask"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     STATIC_URL = "/static/"
+#     STATICFILES_DIRS = [os.path.join(os.path.dirname("app/templates/static"))]
+
 import os
-import app
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/flask"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", "mysql+pymysql://root:@localhost/flask"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_URL = "/static/"
-    STATICFILES_DIRS = [os.path.join(os.path.dirname("app/templates/static"))]
+    STATICFILES_DIRS = [os.path.abspath("app/templates/static")]
