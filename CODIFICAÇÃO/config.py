143f38245e56693374d95eb58af8491aa4bd64b2
tbgbarros <tbg.barros@hotmail.com>
2024-06-03 01:24:03 -0300

    sem erros

--- CODIFICAÇÃO/config.py
+++ CODIFICAÇÃO/config.py
@@ -14,21 +14,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "mysql+pymysql://root:mamaco@localhost/mamaco"
-    )
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/mamaco"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_URL = "/static/"
     STATICFILES_DIRS = [os.path.abspath("app/static")]
-
-
-class ProductionConfig(Config):
-    # Configurações para ambiente de produção no GitHub
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-
-# Seleciona a configuração apropriada com base no ambiente
-if os.environ.get("ENV") == "production":
-    current_config = ProductionConfig
-else:
-    current_config = Config
